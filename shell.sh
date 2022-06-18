fc650677df26550a35d5fb10ff27f336bd88fa12
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-06-18 23:49:44 +0530

    Create shell.sh
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

---
+++ shell.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade -y && sudo apt update && \
+sudo apt install wget curl bash neofetch git apt-utils build-essential software-properties-common libpq-dev python3-dev -y && \
+sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt-get install python3.10.5 python3.10-venv -y && sudo snap install go --classic && sudo apt update && \
+sudo apt autoremove -y && sudo apt update && sudo snap install ngrok && sudo apt update
