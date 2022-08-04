134eac47b7b069df8399f01e94dd88b02950dabf
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-08-04 23:53:23 +0530

    Create shellwithonlypy.sh
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

---
+++ shellwithonlypy.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade -y && sudo apt update && \
+sudo apt install wget curl bash neofetch git apt-utils build-essential software-properties-common python3-dev -y && \
+sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt-get install python3.10 python3.10-venv -y && sudo apt update && \
+sudo apt autoremove -y && sudo apt update
