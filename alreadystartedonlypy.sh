497b8134f7b3ce6b1d7fec28fd45e365182f0583
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-08-24 17:53:11 +0530

    Create alreadystartedonlypy.sh
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

---
+++ alreadystartedonlypy.sh
@@ -0,0 +1,4 @@
+source venv/bin/activate && cd Sagiri && python3.10 -m sagiri &
+source venv/bin/activate && cd Rubina && python3.10 -m alita &
+source venv/bin/activate && cd dmcaprobot && python3.10 main.py &
+source venv/bin/activate && cd pyuserbot && python3.10 main.py &
