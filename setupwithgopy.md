f65b38f612519caa68727e063a0f538f6f3e08bd
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-06-04 09:12:13 +0530

    Update and rename setupwithgopy.sh to setupwithgopy.md
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

---
+++ setupwithgopy.md
@@ -0,0 +1,3 @@
+```
+wget https://gist.githubusercontent.com/annihilatorrrr/973daa28bfae2792cbe11879ef445042/raw/4de70d131534e0b2ee88ab1f462fc7f9fd51edad/updaterwithgopy.sh -O updater.sh; sudo bash ./updater.sh && rm -rf updater.sh && echo "Done!"
+```
--- setupwithgopy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get update && sudo apt-get install wget curl bash neofetch git apt-utils build-essential software-properties-common -y && sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt-get install python3.11 python3.11-dev python3.11-venv -y && sudo snap install go --classic && sudo snap refresh && sudo apt-get update && sudo apt-get autoremove -y && sudo apt update
