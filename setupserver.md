d7a9ffad1bf2bc42f4caedbebffc3f2b39aaad5b
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-06-04 08:56:21 +0530

    Update and rename setupserver.sh to setupserver.md
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

---
+++ setupserver.md
@@ -0,0 +1,3 @@
+```
+wget https://gist.githubusercontent.com/annihilatorrrr/fc7e02174743bfb2f0795d3392855243/raw/2ee2368795ca087d5dd81e093bd5157944421aa5/update.sh -O updater.sh; sudo bash ./updater.sh
+```
--- setupserver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get update && sudo apt-get install wget curl bash neofetch git apt-utils build-essential -y && sudo snap refresh && sudo apt-get update && sudo apt-get autoremove -y && sudo apt update
